9a65b96eb9f2d027baf0b68a75c309faca71a117
Peng Ye <peng783@yahoo.com>
2021-08-04 22:53:19 -0400

    Added typescript.

--- app/ui/navigation/RegisterScreens.js
+++ app/ui/navigation/RegisterScreens.js
@@ -1,9 +1,13 @@
 import { Navigation } from 'react-native-navigation';
+import AccountDetailScreen from "../screen/AccountDetailScreen";
 import HomeScreen from '../screen/HomeScreen';
+import ProjectDetailScreen from "../screen/ProjectDetailScreen";
 import ScreenNames from './ScreenNames';
 
 export const registerScreens = () => {
     Navigation.registerComponent(ScreenNames.HOME_SCREEN, () => HomeScreen);
+    Navigation.registerComponent(ScreenNames.ACCOUNT_DETAIL_SCREEN, () => AccountDetailScreen);
+    Navigation.registerComponent(ScreenNames.PROJECT_DETAIL_SCREEN, () => ProjectDetailScreen);
 };
 
 export default registerScreens;
---
+++ index.ts
@@ -0,0 +1,28 @@
+import { Navigation } from "react-native-navigation";
+import registerScreens from "./app/ui/navigation/RegisterScreens";
+import ScreenNames from './app/ui/navigation/ScreenNames';
+
+registerScreens();
+
+Navigation.events().registerAppLaunchedListener(() => {
+
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: ScreenNames.HOME_SCREEN,
+              options: {
+                topBar: {
+                  visible: false,
+                }
+              }
+            },
+          }
+        ],
+      },
+    },
+ });
+
+});
